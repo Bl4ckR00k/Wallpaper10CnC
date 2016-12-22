4afea8a35f58030acb3a13890ba8be43d1f16b59
Bl4ckR00k <m.maik@live.de>
2016-12-22 13:13:10 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Wallpaper10CnC
+{
+    using System;
+
+    public class Program
+    {
+        private static string Username => @"<username>";
+        private static string SourcePath => @"C:\Users\" + Username + @"\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets";
+        private static string TargetPath => @"C:\Users\" + Username + @"\Pictures";
+        private static string Extension => @".png";
+
+
+        public static void Main(string[] args)
+        {
+            var wpm = new WallpaperManager();
+
+            Console.WriteLine("Starte den Kopier und Vergleichsprozess.");
+
+            var source = wpm.GetSourcePictures(SourcePath);
+            var wallpapersToCopy = wpm.Compare(source, TargetPath);
+
+            wpm.Transfer(wallpapersToCopy, TargetPath, Extension);
+                        
+            Console.WriteLine("Kopiervorgang beendet");
+
+            // wallpapers.FindAndDeleteDoublettes(TargetPath);
+
+            Console.WriteLine("Doubletten entfernt");
+
+            Console.ReadLine();
+        }
+    }
+}
